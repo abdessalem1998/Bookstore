0d3a20c179d183ce00edf9226d75dd79f2695f6e
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-24 22:12:30 +0100

    Use Categories component inside App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
 import Books from './components/Books';
+import Categories from './components/Categories';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Books />
+      <Categories />
     </div>
   );
 }
