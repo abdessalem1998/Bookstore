9d370742a1c4fc97bba4df1d8215cc8ad1ea2be4
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-24 20:02:35 +0100

    Setup Navbar component code and use it in App Component

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = () => (
+    <header>
+      <nav>
+        <h2>Bookstore v1.0</h2>
+        <ul>
+          <li>
+              Books
+          </li>
+          <li>
+              Categories
+          </li>
+        </ul>
+      </nav>
+    </header>
+);
+
+export default Navbar;
