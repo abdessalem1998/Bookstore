7907e35d1e0b34a91234979aff0b9f21ce18d6a9
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-24 20:51:08 +0100

    Setup BookItem Component and use BookItem inside it

---
+++ src/components/BooksComponents/BookList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookItem from './BookItem';
+
+const BookList = () => (
+  <div>
+    <h2>Books list</h2>
+    <BookItem />
+    <BookItem />
+  </div>
+);
+
+export default BookList;
