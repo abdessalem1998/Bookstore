24810eba33bddcb89eaf3e5908fc6f5877800079
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-24 20:43:12 +0100

    Set up BookItem  Component

---
+++ src/components/BooksComponents/BookItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookItem = () => (
+  <div>
+    <h2>Book title</h2>
+    <p>des</p>
+    <button type="button">Delete</button>
+  </div>
+);
+
+export default BookItem;
