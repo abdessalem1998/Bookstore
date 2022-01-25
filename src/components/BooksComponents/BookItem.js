a2c1e613e72ebfd3608b9c09c13f3176f34dc9a4
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-25 14:47:11 +0100

    dynamically map book items

--- src/components/BooksComponents/BookItem.js
+++ src/components/BooksComponents/BookItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const BookItem = () => (
-  <div>
-    <h3>Book title</h3>
-    <p>Category</p>
-    <button type="button">Delete</button>
-  </div>
-);
+const BookItem = (book) => {
+  const { id, title, author } = book;
+  return (
+    <div id={id}>
+      <h3>{title}</h3>
+      <p>{author}</p>
+      <button type="button">Delete</button>
+    </div>
+  );
+};
 
 export default BookItem;
