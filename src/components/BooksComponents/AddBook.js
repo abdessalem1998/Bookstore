97763d6bf233e3161a16ddd2e53b670ac92356e6
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-26 14:12:15 +0100

    implement  submit handler and dispatch its action

--- src/components/BooksComponents/AddBook.js
+++ src/components/BooksComponents/AddBook.js
@@ -7,10 +7,18 @@ import { v4 as uuidv4 } from 'uuid';
 const AddBook = () => {
   const [author, setAuthor] = useState('');
   const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addBook({
+      id: uuidv4(), title, author,
+    }));
+  };
+
   return (
     <div>
       <h2>Add New Book</h2>
-      <form className="form">
+      <form onSubmit={handleSubmit}>
         <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" required />
         <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Author" required />
         <button type="submit">Add Book</button>
