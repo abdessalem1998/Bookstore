1239268393064cbc53e1a836c99e40978292e1c5
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-27 16:34:06 +0100

    fetch initial data from api

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,6 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const FETCH_BOOK = 'bookStore/books/FETCH_BOOK';
 
 const initialState = [];
 
@@ -13,12 +14,19 @@ export const removeBook = (payload) => ({
   payload,
 });
 
+export const fetchBook = (payload) => ({
+  type: FETCH_BOOK,
+  payload,
+});
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
       return [...state.filter((book) => book.id !== action.payload)];
+    case FETCH_BOOK:
+      return action.payload;
     default:
       return state;
   }
