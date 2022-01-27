4d804c06e222deafaaa5ecd3bd9d8f9b52596b66
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2022-01-27 23:17:39 +0100

    Delete data from Api and update the display in UI

--- src/components/BooksComponents/BookItem.js
+++ src/components/BooksComponents/BookItem.js
@@ -5,8 +5,23 @@ import { removeBook } from '../../redux/books/books';
 const BookItem = (book) => {
   const dispatch = useDispatch();
   const { id, title, category } = book;
+
+  const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/';
+  const APPID = '1bssb58ufarxq1Bmhi6i';
+
+  const deleteData = async () => {
+    const response = await fetch(`${URL}${APPID}/books/${id}/`, {
+      method: 'Delete',
+    });
+    return response;
+  };
+
   const removeHandler = () => {
-    dispatch(removeBook(id));
+    deleteData().then((response) => {
+      if (response.ok) {
+        dispatch(removeBook(id));
+      }
+    });
   };
   return (
     <div>
